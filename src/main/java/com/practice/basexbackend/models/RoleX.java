024be09584a73b2a0365068ba15944be02f99720
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-06-29 14:22:28 +0300

    entity and app properties done

--- src/main/java/com/practice/basexbackend/models/RoleX.java
+++ src/main/java/com/practice/basexbackend/models/RoleX.java
@@ -1,4 +1,23 @@
 package com.practice.basexbackend.models;
 
+import com.practice.basexbackend.enumeration.ERole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter @RequiredArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "roles")
 public class RoleX {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole roleName;
 }
