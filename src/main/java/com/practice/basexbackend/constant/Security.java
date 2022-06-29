a142fd6fd30354f461a110e82e10171919d8e9a0
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-06-29 20:35:17 +0300

    auth-controller ready

--- src/main/java/com/practice/basexbackend/constant/Security.java
+++ src/main/java/com/practice/basexbackend/constant/Security.java
@@ -13,5 +13,6 @@ public class Security {
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this info";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
-    public static final String[] PUBLIC_URLS = {"/api/auth/signup", "/api/auth/login", "/api/auth/token/refresh", "/api/users"};
+    public static final String[] PUBLIC_URLS = {"/api/auth/**"};
+    public static final String[] PRIVATE_URLS = {"/api/**"};
 }
