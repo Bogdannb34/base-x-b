c2597666965a0a83ace7ca18524e8871491b78cd
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-06-29 12:43:11 +0300

    initial commit

---
+++ src/main/java/com/practice/basexbackend/BaseXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.basexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseXBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/basexbackend/BaseXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.basexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
